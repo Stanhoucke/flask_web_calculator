f8ac8b33ecb91f8d33393382429841199904559a
Stanley Houcke <stanhoucke@gmail.com>
2020-12-02 17:52:22 +0000

    added a route

--- flask_calculator/controllers/controller.py
+++ flask_calculator/controllers/controller.py
@@ -1,5 +1,14 @@
 from app import app
+from modules.calculator import Calculator
+
+calculator = Calculator()
 
 @app.route("/")
 def index():
-    return "This is a webpage"
+    return "Use the URL to perform calculator functions"
+
+@app.route("/add/<number_1>,<number_2>")
+def add(number_1, number_2):
+    integer_1 = int(number_1)
+    integer_2 = int(number_2)
+    return f"The answer is: {calculator.add(integer_1, integer_2)}"
